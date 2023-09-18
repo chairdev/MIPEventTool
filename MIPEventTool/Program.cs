88b34f0c2c53e97951c8e0aa7f0e6916f91fa63f
Taireru Benaru <38292283+chairdev@users.noreply.github.com>
2023-09-18 10:10:04 +0200

    create project

---
+++ MIPEventTool/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//check if the user typed unpack [filename] [output directory]
+using MIPEventTool;
+
+if (args[0] == "unpack")
+    {
+        //check if the file exists
+        if (File.Exists(args[1]))
+        {
+            //check if the output directory exists
+            if (Directory.Exists(args[2]))
+            {
+                Unpacker unpacker = new Unpacker(args[1], args[2], UnpackerType.Dialogue);
+            }
+            else
+            {
+                //output directory does not exist
+                Console.WriteLine("Output directory does not exist");
+            }
+        }
+        else
+        {
+            //file does not exist
+            Console.WriteLine("File does not exist");
+        }
+    }
+    else
+    {
+        //user did not type unpack
+        Console.WriteLine("Please type unpack [filename] [output directory]");
+    }
---
+++ MIPEventTool/Unpacker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+namespace MIPEventTool
+{
+	public class Unpacker
+	{
+		//unpack file using the table
+		public Unpacker(string filename, string output, UnpackerType type)
+		{
+			switch(type)
+			{
+				case UnpackerType.Dialogue:
+					UnpackDialogue(filename, output);
+				break;
+			}
+		}
+
+		public void UnpackDialogue(string filename, string output)
+		{
+			//read file
+			byte[] bin = File.ReadAllBytes(filename);
+			string filenameWithoutExtension = Path.GetFileNameWithoutExtension(filename);
+
+			//use the table to unpack
+			string text = "";
+			for(int i = 0; i < bin.Length; i++)
+			{
+				//if the byte is in the table, add it to the string
+				if(CharacterTable.DialogueTable.ContainsKey(bin[i]))
+				{
+					text += CharacterTable.DialogueTable[bin[i]];
+				}
+				//if it's not in the table, add a space
+				else
+				{
+					text += "?";
+				}
+			}
+
+			//write the string to a file
+			File.WriteAllText(output + filenameWithoutExtension + ".txt", text);
+
+		}
+	}
+}
+
+public enum UnpackerType
+{
+	Dialogue,
+	Menu,
+}
+

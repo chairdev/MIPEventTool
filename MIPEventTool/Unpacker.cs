f03acde8aad532e8b37a08e9990bf92e313b0b22
Taireru Benaru <38292283+chairdev@users.noreply.github.com>
2023-10-03 11:19:07 +0200

    actually saves text

--- MIPEventTool/Unpacker.cs
+++ MIPEventTool/Unpacker.cs
@@ -1,50 +1,54 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace MIPEventTool
 {
-	public class Unpacker
-	{
+    public class Unpacker
+    {
         public uint basePointer = 0x80100000;
 
         //unpack file using the table
         public Unpacker(string filename, string output, UnpackerType type)
-		{
-			switch(type)
-			{
-				case UnpackerType.Dialogue:
-					UnpackDialogue(filename, output);
-				break;
-			}
-		}
+        {
+            switch (type)
+            {
+                case UnpackerType.Dialogue:
+                    UnpackDialogue(filename, output);
+                    break;
+            }
+        }
 
-		public void UnpackDialogue(string filename, string output)
-		{
-			Console.WriteLine("hi");
-			//read file
-			byte[] bin = File.ReadAllBytes(filename);
-			string filenameWithoutExtension = Path.GetFileNameWithoutExtension(filename);
+        public void UnpackDialogue(string filename, string output)
+        {
+            Console.WriteLine("hi");
+            //read file
+            byte[] bin = File.ReadAllBytes(filename);
+            string filenameWithoutExtension = Path.GetFileNameWithoutExtension(filename);
 
-            
-            Dictionary<int, int> strings = new Dictionary<int, int>();
-			Dictionary<int, byte> ranges = new Dictionary<int, byte>();
 
-			for (int i = 0; i < bin.Length-8; i+=4)
-			{
+            List<string> strings = new List<string>();
+            Dictionary<int, byte> ranges = new Dictionary<int, byte>();
+
+            for (int i = 0; i < bin.Length - 8; i += 4)
+            {
                 UInt16 val = BitConverter.ToUInt16(bin, i);
 
                 if (val == 0x55FF)
-				{
+                {
                     //get 32 bit value
                     int value = BitConverter.ToInt32(bin, i + 4);
                     int pointer = ((int)(value - basePointer));
-					if(IsValidPointer(value, bin.Length))
+                    if (IsValidPointer(value, bin.Length))
                     {
                         //copy into from bin into res from pointer
                         byte[] res = bin.SubArray(8, (int)pointer);
                         //Array.Copy(bin, res, (int)value);
-                        if(ranges.ContainsKey((int)pointer))
+                        if (ranges.ContainsKey((int)pointer))
                         {
                             ranges[(int)pointer] = res[0];
                         }
@@ -52,13 +56,13 @@ namespace MIPEventTool
                         {
                             ranges.Add((int)pointer, res[0]);
                         }
-                        
+
 
                         //DecodeDialogue(res, filenameWithoutExtension + i);
                     }
-					
-				}
-			}
+
+                }
+            }
 
             foreach (var entry in ranges)
             {
@@ -69,11 +73,13 @@ namespace MIPEventTool
                 Array.Copy(bin, 8 + pointer, data, 0, data.Length);
 
                 // Decode the extracted data using your DecodeDialogue function
-                DecodeDialogue(data, $"{filenameWithoutExtension}_{pointer}.bin");
+                strings.Add(DecodeDialogue(bin, 8 + pointer));
             }
 
-				
-		}
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(strings, options);
+            File.WriteAllText(Environment.CurrentDirectory + "/output/" + $"{filenameWithoutExtension}_text.json", jsonString);
+        }
 
         bool IsValidPointer(int ptr, int length)
         {
@@ -82,67 +88,75 @@ namespace MIPEventTool
             return value;
         }
 
-		void DecodeDialogue(byte[] bin, string filenameWithoutExtension)
-		{
+        string DecodeDialogue(byte[] bin, int startIndex = 0)
+        {
             string text = "";
+            bool finished = false;
             //start of persona game text = 0x00002500
-            for (int j = 0; j < bin.Length; j++)
+            for (int j = startIndex; j < bin.Length; j++)
             {
-                if (bin[j] == 0x80)
+                if (!finished)
                 {
-                    byte otherByte = bin[j + 1];
-                    ushort key = (ushort)(bin[j] << 8 | otherByte);
-                    //write like 0x80A5 etc
-                    //Console.WriteLine("0x" + key.ToString("X4"));
-
-                    if (CharacterTable.DialogueTable.ContainsKey(key))
+                    if (bin[j] == 0x80)
                     {
-                        text += CharacterTable.DialogueTable[key];
-                        j++;
-                    }
-                }
-                else if (bin[j] == 0xFF)
-                {
-                    //total command = FF 05 XX 00
-                    switch (bin[j + 1])
-                    {
-                        case 0x01:
-                        case 0x02:
-                        case 0x03:
-                        case 0x04:
-                            text += CharacterTable.DialogueTable[(ushort)(bin[j] << 8 | bin[j + 1])];
-                            j += 1;
-                        break;
-                        case 0x05:
-                            byte time = bin[j + 2];
-                            text += CharacterTable.DialogueTable[(ushort)(bin[j] << 8 | bin[j + 1])] + time + "]";
-                            j += 2;
-                        break;
-                        case 0x06:
-                        case 0x07:
-                        case 0x08:
-                        case 0x0E:
-                        case 0x0F:
-                            byte arg = bin[j + 2];
-                            text += CharacterTable.DialogueTable[(ushort)(bin[j] << 8 | bin[j + 1])] + arg + "]";
-                            j += 1;
-                         break;
-                    }
+                        byte otherByte = bin[j + 1];
+                        ushort key = (ushort)(bin[j] << 8 | otherByte);
+                        //write like 0x80A5 etc
+                        //Console.WriteLine("0x" + key.ToString("X4"));
 
-                }
-                else if (CharacterTable.DialogueTable.ContainsKey(bin[j]))
-                {
-                    text += CharacterTable.DialogueTable[bin[j]];
-                }
-                //if it's not in the table, write [XX]
-                else
-                {
-                    text += "[" + bin[j].ToString("X2") + "]";
+                        if (CharacterTable.DialogueTable.ContainsKey(key))
+                        {
+                            text += CharacterTable.DialogueTable[key];
+                            j++;
+                        }
+                    }
+                    else if (bin[j] == 0xFF)
+                    {
+                        //total command = FF 05 XX 00
+                        switch (bin[j + 1])
+                        {
+                            case 0x01:
+                                finished = true;
+                                text += CharacterTable.DialogueTable[(ushort)(bin[j] << 8 | bin[j + 1])];
+                                j += 1;
+                                break;
+                                break;
+                            case 0x02:
+                            case 0x03:
+                            case 0x04:
+                                text += CharacterTable.DialogueTable[(ushort)(bin[j] << 8 | bin[j + 1])];
+                                j += 1;
+                                break;
+                            case 0x05:
+                                byte time = bin[j + 2];
+                                text += CharacterTable.DialogueTable[(ushort)(bin[j] << 8 | bin[j + 1])] + time + "]";
+                                j += 2;
+                                break;
+                            case 0x06:
+                            case 0x07:
+                            case 0x08:
+                            case 0x0E:
+                            case 0x0F:
+                                byte arg = bin[j + 2];
+                                text += CharacterTable.DialogueTable[(ushort)(bin[j] << 8 | bin[j + 1])] + arg + "]";
+                                j += 1;
+                                break;
+                        }
+
+                    }
+                    else if (CharacterTable.DialogueTable.ContainsKey(bin[j]))
+                    {
+                        text += CharacterTable.DialogueTable[bin[j]];
+                    }
+                    //if it's not in the table, write [XX]
+                    else
+                    {
+                        text += "[" + bin[j].ToString("X2") + "]";
+                    }
                 }
             }
-            File.WriteAllText(Environment.CurrentDirectory + "/output/" + filenameWithoutExtension + ".txt", text);
+            return text;
         }
-
 
         void UnpackSectors(byte[] bin, string filenameWithoutExtension)
         {
@@ -165,10 +179,10 @@ namespace MIPEventTool
                 int start = sectors[i];
                 int end = sectors[i + 1];
                 byte[] data = bin.SubArray(start * 0x800, end * 0x800);
-                DecodeDialogue(data, filenameWithoutExtension + i);
+                //DecodeDialogue(data, filenameWithoutExtension + i);
             }
         }
-	}
+    }
 }
 
 public enum UnpackerType
